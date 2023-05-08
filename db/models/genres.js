573528bfa5d446ce57f304c976a3eb5b168cbfe4
Sumit Savaliya <sumit@freshprints.com>
2023-05-08 10:11:53 +0530

    movie routes and model

--- db/models/genres.js
+++ db/models/genres.js
@@ -5,6 +5,8 @@ const genreSchema = new mongoose.Schema({
   name: {
     type: String,
     required: true,
+    minLength: 5,
+    maxLength: 255,
   },
 });
 
@@ -12,11 +14,12 @@ const Genre = mongoose.model('Genre', genreSchema);
 
 function validateGenre(genre) {
   const schema = Joi.object({
-    name: Joi.string().min(3).required(),
+    name: Joi.string().min(5).max(255).required(),
   });
 
   return schema.validate(genre);
 }
 
+module.exports.genreSchema = genreSchema;
 module.exports.Genre = Genre;
 module.exports.validate = validateGenre;
