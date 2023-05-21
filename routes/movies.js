07518a4396f445e0d8d48dce0b65ce1e52d0b0d4
Sumit <sumit@Sumits-MacBook-Pro.local>
2023-05-21 08:34:08 +0530

    implemented auth middleware

--- routes/movies.js
+++ routes/movies.js
@@ -1,18 +1,19 @@
-const router = require('express').Router();
-const { Movie, validateMovie } = require('../db/models/movies');
-const { Genre } = require('../db/models/genres');
+const router = require("express").Router();
+const { Movie, validateMovie } = require("../db/models/movies");
+const { Genre } = require("../db/models/genres");
+const auth = require("../middleware/auth");
 
-router.get('/', async (req, res) => {
-  const movies = await Movie.find().sort('title');
+router.get("/", async (req, res) => {
+  const movies = await Movie.find().sort("title");
   res.send(movies);
 });
 
-router.post('/', async (req, res) => {
+router.post("/", auth, async (req, res) => {
   const { error } = validateMovie(req.body);
   if (error) return res.status(400).send(error.details[0].message);
 
   const genre = await Genre.findById(req.body.genreId);
-  if (!genre) return res.status(400).send('Invalid Genre');
+  if (!genre) return res.status(400).send("Invalid Genre");
 
   let movie = new Movie({
     ...req.body,
@@ -24,14 +25,14 @@ router.post('/', async (req, res) => {
   res.send(movie);
 });
 
-router.put('/:id', async (req, res) => {
-  if (req.params.id.length != 24) return res.status(400).send('Invalid Id');
+router.put("/:id", auth, async (req, res) => {
+  if (req.params.id.length != 24) return res.status(400).send("Invalid Id");
 
   const { error } = validate(req.body);
   if (error) return res.status(400).send(error.details[0].message);
 
   const genre = await Genre.findById(req.body.genreId);
-  if (!genre) return res.status(400).send('Invalid genre.');
+  if (!genre) return res.status(400).send("Invalid genre.");
 
   const movie = await Movie.findByIdAndUpdate(
     req.params.id,
@@ -48,29 +49,29 @@ router.put('/:id', async (req, res) => {
   );
 
   if (!movie)
-    return res.status(404).send('The movie with the given ID was not found.');
+    return res.status(404).send("The movie with the given ID was not found.");
 
   res.send(movie);
 });
 
-router.delete('/:id', async (req, res) => {
-  if (req.params.id.length != 24) return res.status(400).send('Invalid Id');
+router.delete("/:id", auth, async (req, res) => {
+  if (req.params.id.length != 24) return res.status(400).send("Invalid Id");
 
   const movie = await Movie.findByIdAndRemove(req.params.id);
 
   if (!movie)
-    return res.status(404).send('The movie with the given ID was not found.');
+    return res.status(404).send("The movie with the given ID was not found.");
 
   res.send(movie);
 });
 
-router.get('/:id', async (req, res) => {
-  if (req.params.id.length != 24) return res.status(400).send('Invalid Id');
+router.get("/:id", async (req, res) => {
+  if (req.params.id.length != 24) return res.status(400).send("Invalid Id");
 
   const movie = await Movie.findById(req.params.id);
 
   if (!movie)
-    return res.status(404).send('The movie with the given ID was not found.');
+    return res.status(404).send("The movie with the given ID was not found.");
 
   res.send(movie);
 });
--- routes/rentals.js
+++ routes/rentals.js
@@ -1,27 +1,28 @@
-const router = require('express').Router();
-const { Movie } = require('../db/models/movies');
-const { Genre } = require('../db/models/genres');
-const { Customer } = require('../db/models/customers');
-const { Rental, validateRental } = require('../db/models/rentals');
-const mongoose = require('mongoose');
+const router = require("express").Router();
+const { Movie } = require("../db/models/movies");
+const { Genre } = require("../db/models/genres");
+const { Customer } = require("../db/models/customers");
+const { Rental, validateRental } = require("../db/models/rentals");
+const auth = require("../middleware/auth");
+const mongoose = require("mongoose");
 
-router.get('/', async (req, res) => {
-  const rentals = await Rental.find().sort('-dateOut');
+router.get("/", async (req, res) => {
+  const rentals = await Rental.find().sort("-dateOut");
   res.send(rentals);
 });
 
-router.post('/', async (req, res) => {
+router.post("/", auth, async (req, res) => {
   const { error } = validateRental(req.body);
   if (error) return res.status(400).send(error.details[0].message);
 
   const customer = await Customer.findById(req.body.customerId);
-  if (!customer) return res.status(400).send('Invalid Customer');
+  if (!customer) return res.status(400).send("Invalid Customer");
 
   const movie = await Movie.findById(req.body.movieId);
-  if (!movie) return res.status(400).send('Invalid Movie');
+  if (!movie) return res.status(400).send("Invalid Movie");
 
   if (movie.numberInStock === 0)
-    return res.status(400).send('Movie not in stock');
+    return res.status(400).send("Movie not in stock");
 
   let rental = new Rental({
     customer: customer,
